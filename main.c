86fb26c78d370cfababd716d6c606a2c787a0abd
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-11-13 10:59:09 -0500

    formatting, figuring stuff out

--- main.c
+++ main.c
@@ -9,13 +9,13 @@ int main() {
   printf("Kiran Vuksanaj - Work 05 || \"I need information, STAT!\"\n");
   struct stat statbuf;
   int status;
-  char *filepath = "makefile";
-  
+  char *filepath = "./makefile";
+
   status = stat(filepath,&statbuf);
   if(status < 0) {
     printf("Error %d while statting file: %s",errno,strerror(errno));
     return 1;
   }
-  printf("Successful stat of file!\n");
-  printf("Mode of file: %o\n",statbuf.st_mode);
+  printf("metadata for [%s]\n",filepath);
+  printf("mode of file: %o\n",statbuf.st_mode);
 }
