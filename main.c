b5402dfca754f2f15b482b570e3edbbe527ff247
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-11-13 14:02:01 -0500

    print last access time in seconds

--- main.c
+++ main.c
@@ -9,13 +9,15 @@ int main() {
   printf("Kiran Vuksanaj - Work 05 || \"I need information, STAT!\"\n");
   struct stat statbuf;
   int status;
-  char *filepath = "./makefile";
-
+  char *filepath = "./main.c";
+  // stuff
   status = stat(filepath,&statbuf);
   if(status < 0) {
     printf("Error %d while statting file: %s",errno,strerror(errno));
     return 1;
   }
   printf("metadata for [%s]\n",filepath);
+  printf("filesize: %ld\n",statbuf.st_size);
   printf("mode of file: %o\n",statbuf.st_mode);
+  printf("last accessed: %lu\n",statbuf.st_atim.tv_sec);
 }
