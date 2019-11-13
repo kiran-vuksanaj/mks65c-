79c4818ef4edf56efaf91b886e416be3e5062eb6
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-11-13 10:56:43 -0500

    successful stat of makefile

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<sys/stat.h>
+#include<time.h>
+#include<string.h>
+#include<errno.h>
 
 int main() {
   printf("Kiran Vuksanaj - Work 05 || \"I need information, STAT!\"\n");
+  struct stat statbuf;
+  int status;
+  char *filepath = "makefile";
+  
+  status = stat(filepath,&statbuf);
+  if(status < 0) {
+    printf("Error %d while statting file: %s",errno,strerror(errno));
+    return 1;
+  }
+  printf("Successful stat of file!\n");
+  printf("Mode of file: %o\n",statbuf.st_mode);
 }
